25e5ecd685d8d321e05c030ce249bc870f7ea4de
Essan Alshakerchi <essan.alshakerchi@iths.se>
2026-05-11 01:24:14 +0200

    Api controller endpoint fix

---
+++ AzureAppServiceAPI/Controllers/ApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAppServiceAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetRoot()
+        {
+            return Ok(new { message = "Welcome to Azure App Service API", version = "1.0" });
+        }
+    }
+}
