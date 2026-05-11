081e4c55f5fdedf526280ea0297194d30bb4754e
Essan Alshakerchi <essan.alshakerchi@iths.se>
2026-05-11 14:31:29 +0200

    test api

---
+++ AzureAppServiceAPI/Controllers/ApiCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAppServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetRoot()
+        {
+            return Ok(new { message = "Krya på dig fredrik" });
+        }
+    }
+}
